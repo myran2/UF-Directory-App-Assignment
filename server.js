b02ea54eec1007d5516fc11c6104dc0cc6b30949
Henry Gordon <henrytgordon@gmail.com>
2017-08-23 08:48:33 -0500

    Implement required listings functionality

--- server.js
+++ server.js
@@ -6,21 +6,29 @@ var http = require('http'),
 /* Global variables */
 var listingData, server;
 
+// Send listings data if on the listings page. 404 otherwise
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
+  if (parsedUrl.path == "/listings") {
+    response.writeHead(200, {
+      'Content-Length': Buffer.byteLength(listingData),
+      'Content-Type': 'text/plain'
+    });
+    response.write(listingData);
+  }
+  else {
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("Bad gateway error");
+  }
 
-  /*
-    Your request handler should send listingData in the JSON format if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: explore the request object and its properties 
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-   */
+  response.end();
 };
 
+// read listings data into memory and then start the server
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-   */
+  listingData = data;
+  server = http.createServer(requestHandler);
+  server.listen(port, function() {
+    console.log('Server listening on: http://127.0.0.1:' + port);
+  });
 });
